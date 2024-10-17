57ebc919b5a53c3e729354a4074c8b3b4d921ab1
Mahir Hossain <166791201+toxic1320@users.noreply.github.com>
2024-10-17 21:05:48 +0600

    mahir
    mahir

---
+++ Question No-2/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Nav from './components/Nav'
+import Home from './pages/Home'
+import Footer from './components/Footer'
+function App() {
+
+
+  return (
+    <>
+    <div><Nav/></div>
+<div><Home/></div>
+<Footer/>
+
+    </>
+  )
+}
+
+export default App
